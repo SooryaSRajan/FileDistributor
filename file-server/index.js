c7cb999fac80024e7637b30eec152660a8b38e3c
sooryasrajan <soorya.s27@gmail.com>
2022-11-10 14:25:30 +0530

    Added web directory

--- file-server/index.js
+++ file-server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const app = express()
+const path = require("path")
 const PORT = process.env.PORT || 8000
 
 //routes import
@@ -10,6 +11,7 @@ const fileManagementRoute = require("./routes/file-management")
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser())
+app.use('/', express.static(path.join(__dirname, 'web')))
 
 //routes
 app.use("/files", fileManagementRoute)
